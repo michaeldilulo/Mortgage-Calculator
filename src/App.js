1630f8be8e8a7da81c29e9646c3ccce67d3a4807
michael dilulo <snoleopard01001101@gmail.com>
2021-02-23 10:10:11 -0700

    Form / Header / App. Imports etc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Form from "./Form.js"
+import Header from "./Header.js"
 
 function App() {
   return (
     <div className="app">
-<Form />
+      <Header />
+      <div className="app__form">
+      <Form />
+      </div>
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { FormControl, InputLabel, Input, FormHelperText, Button } from '@material-ui/core'
 import "./Form.css"
 
 function Form() {
     return (
         <div className="form">
-            <h1>Hello World</h1>
+  <FormControl>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+                <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+                <Button color="primary" variant="contained">Calculate</Button>
+</FormControl>
         </div>
     )
 }
